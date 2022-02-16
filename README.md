c7b854151032d7edb537883e86c613d764d27364
Butch Mayhew <bmayhew@tilled.com>
2022-02-16 09:15:15 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Playwright Init Base
+
+This repo was built using the [npm init command](https://playwright.dev/docs/intro#using-init-command).
+
+The only modifications were commenting out the different browser runs, adding an test/api directory and creating an api test example.
---
+++ tests/api/example.spec.ts
@@ -0,0 +1,19 @@
+import { test, expect } from "@playwright/test";
+
+test("GET todos/1 example", async ({ request }) => {
+  const response = await request.get(
+    "https://jsonplaceholder.typicode.com/todos/1",
+    {
+      headers: {
+        Accept: "*/*",
+        ContentType: "application/json",
+      },
+    }
+  );
+  const body = JSON.parse(await response.text());
+  expect(response.status()).toBe(200);
+  expect(body.userId).toBe(1);
+  expect(body.id).toBe(1);
+  expect(body.title).toBe("delectus aut autem");
+  expect(body.completed).toBe(false);
+});
