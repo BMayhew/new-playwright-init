7bccee6f01b2e1979ff90bfc96138880338588fd
Butch Mayhew <bmayhew@tilled.com>
2023-02-04 18:04:00 -0600

    updating with some html test annotations

--- tests/ui/example.spec.ts
+++ tests/ui/example.spec.ts
@@ -1,11 +1,28 @@
 import { test, expect } from "@playwright/test";
 
-test("basic test", async ({ page }) => {
-  await page.goto("http://127.0.0.1:3000");
+test.describe("Test Title", async () => {
+  test("Test Name", async ({ page }, testInfo) => {
+    await test.step("Test Step 1", async () => {
+      await page.goto("http://127.0.0.1:3000");
+      const screenshot = await page.screenshot();
+      await testInfo.attach("screenshot", {
+        body: screenshot,
+        contentType: "image/png",
+      });
+    });
 
-  await page.getByTestId("username").fill("Butch");
-  await page.getByTestId("password").fill("Password");
-  await page.getByTestId("login").click();
+    await test.step("Test Step 2", async () => {
+      test.info().annotations.push({
+        type: "issue",
+        description:
+          "https://github.com/microsoft/playwright/issues/<some-issue>",
+      });
+      await page.getByTestId("username").fill("Butch");
+    });
 
-  // await expect(page).toHaveTitle(/Getting started/);
+    await page.getByTestId("password").fill("Password");
+    await page.getByTestId("login").click();
+
+    // await expect(page).toHaveTitle(/Getting started/);
+  });
 });
